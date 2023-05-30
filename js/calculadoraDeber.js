a7724a59190266d1e04150c3a901c215a256f9b5
Juan Jimenez <jijimenez@uce.edu.ec>
2023-05-30 14:54:50 -0500

    Practica

---
+++ js/calculadoraDeber.js
@@ -0,0 +1,24 @@
+
+function num(numero){
+    var number = numero; 
+    document.getElementById("resultado").value =
+        document.getElementById("resultado").value + numero;
+}
+
+function suma(){
+    var numero1=parseInt(document.getElementById("resultado").value)
+    document.getElementById("resultado").innerHTML=""
+
+}
+
+function resta(){
+    var numero1=parseInt(document.getElementById("resultado").value)
+}
+
+function multiplicacion(){
+    var numero1=parseInt(document.getElementById("resultado").value)
+}
+
+function division(){
+    var numero1=parseInt(document.getElementById("resultado").value)
+}
