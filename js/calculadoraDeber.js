9ad54897dd2fceca428e921526e13333ac8a7d19
David-Masabanda <josue.masabanda99@gmail.com>
2023-05-31 12:26:12 -0500

    Deber4

--- js/calculadoraDeber.js
+++ js/calculadoraDeber.js
@@ -1,24 +1,38 @@
+let num1;
+let num2;
+let operacion;
 
 function num(numero){
-    var number = numero; 
     document.getElementById("resultado").value =
         document.getElementById("resultado").value + numero;
 }
 
-function suma(){
-    var numero1=parseInt(document.getElementById("resultado").value)
-    document.getElementById("resultado").innerHTML=""
-
+function convertir(idCampo){
+    return parseInt(document.getElementById(idCampo).value);
 }
 
-function resta(){
-    var numero1=parseInt(document.getElementById("resultado").value)
+function op(funcion){
+    operacion= funcion
+    num1=convertir('resultado')
+    console.log(num1);
+    document.getElementById("resultado").value=''
 }
 
-function multiplicacion(){
-    var numero1=parseInt(document.getElementById("resultado").value)
+
+function igual(){  
+    num2=convertir('resultado')
+    console.log(num2);
+    resolver(num1, num2)
 }
 
-function division(){
-    var numero1=parseInt(document.getElementById("resultado").value)
+function resolver(numero1, numero2){
+    var result=operacion(numero1,numero2)
+    document.getElementById("resultado").value=result
+}
+
+function resetear(){
+    num1=0
+    num2=0
+    operacion=undefined
+    document.getElementById("resultado").value=''
 }
